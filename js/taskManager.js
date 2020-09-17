833011bbb92316b558adb0b6621fdf4dbd8a177a
irina <irina.kose@gmail.com>
2020-09-17 11:33:30 +1000

    task 7 steps 1 and 2 taskManager.js and index.html update

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,3 +1,20 @@
+
+//7.1: Create new function createTaskHtml
+const createTaskHtml = (taskName, taskDescription, assignedTo, dueDate, status) => `
+<li class="list-group-item">
+<div class="d-flex w-100 mt-2 justify-content-between align-items-center">
+    <h5>${taskName}</h5>
+    <span class="badge badge-danger">${status}</span>
+</div>
+<div class="d-flex w-100 mb-3 justify-content-between">
+    <small>Assigned To: ${assignedTo}</small>
+    <small>Due: ${dueDate}</small>
+</div>
+<p>${taskDescription}</p>
+</li>
+`
+
+
 //create taskManager class
 class TaskManager {
     constructor(currentId = 0) {
@@ -16,8 +33,31 @@ addTask (taskName, taskDescription, assignedTo, dueDate) {
     };
     this.tasks.push(task);
 }
+//7.2: Create render() method
+render() {
+    //Create a variable storing an empty array to hold the HTML of all the tasks
+const taskHtmlList = [];
+    //Loop over the TaskManager's tasks, for each task
+    for (let i = 0; i < this.tasks.length; i++) {
+        
+        //Store the current task in a variable
+        const task = this.thasks[i];
+        //Create a formattedDate variable, storing a readable string representing the date
+        const date = new Date(task.dueDate);
+        const formattedDate = date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+        //Create task html
+        const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.status);
+        //Push the taskHtml into the tasksHtmlList array
+        taskHtmlList.push(taskHtml);
+    }
+    //Cteate tasksHtml 
+    const tasksHtml = taskHtmlList.join('\n');
+    
+    //Select the tasks list element and set its innerHTML to the tasksHtml
+    const tasksList = document.querySelector('#taskList');
+    tasksList.innerHTML = tasksHtml;
 }
-
+}
 
 
 
